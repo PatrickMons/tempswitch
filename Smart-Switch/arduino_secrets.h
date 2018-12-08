036e0fc7cb66333c1977844ac344e13e8677cfd7
PatrickMons <45695040+PatrickMons@users.noreply.github.com>
2018-12-08 18:30:42 +0100

    Update arduino_secrets.h

--- Smart-Switch/arduino_secrets.h
+++ Smart-Switch/arduino_secrets.h
@@ -1,4 +1,4 @@
 #define SECRET_SSID "Vodafone-A84166271"
 #define SECRET_PASS "ilgattorotola"
 #define SECRET_TOKEN "495700982:AAEWW_edUOKUaVrx4V46ag4HCFHK8UExToU"
-#define SECRET_LINK "mioprimotest.herokuapp.com"
+#define SECRET_LINK "provaswitch.herokuapp.com"
